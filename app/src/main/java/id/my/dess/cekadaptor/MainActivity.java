c9cef590b89e4c4ffd87ceeac1fc24ad3f66dcd0
DENNI RAMADIANTO <raysdenni@gmail.com>
2022-07-09 23:40:28 +0700

    Design Complete

--- app/src/main/java/id/my/dess/cekadaptor/MainActivity.java
+++ app/src/main/java/id/my/dess/cekadaptor/MainActivity.java
@@ -1,9 +1,19 @@
 package id.my.dess.cekadaptor;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -15,7 +25,9 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     //variable nilai sensor
-    TextView voltageh, currenth, powerh, quality;
+    TextView voltageh, currenth, powerh, quality, statusadaptor;
+    LinearLayout qualitybg;
+    ImageView dotstatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,9 @@ public class MainActivity extends AppCompatActivity {
         currenth = findViewById(R.id.currenth);
         powerh = findViewById(R.id.powerh);
         quality = findViewById(R.id.quality);
-
+        qualitybg = findViewById(R.id.qualitybg);
+        dotstatus = findViewById(R.id.dotstatus);
+        statusadaptor = findViewById(R.id.statusadaptor);
 
         //Koneksi database
         DatabaseReference koneksi = FirebaseDatabase.getInstance().getReference();
@@ -38,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 //Tampilkan nilai sensor kedalam aplikasi
                 //Baca nilai sensor
+
                 String voltase = snapshot.child("voltase").getValue().toString();
                 String ampere = snapshot.child("ampere").getValue().toString();
                 String daya = snapshot.child("daya").getValue().toString();
@@ -48,14 +63,34 @@ public class MainActivity extends AppCompatActivity {
                 powerh.setText(daya);
 
                 //Uji nilai sensor untuk menentukan kualitas
-                if (Float.parseFloat(voltase) <= 2)
-                    quality.setText("Tidak terhubung");
-                else if (Float.parseFloat(voltase) > 2 && Float.parseFloat(voltase) < 18.05)
-                    quality.setText("Under Voltage");
-                else if (Float.parseFloat(voltase) >18.05 && Float.parseFloat(voltase) <19.95)
-                    quality.setText("Normal");
-                else
-                    quality.setText("Over Voltage");
+                if (Float.parseFloat(voltase) <= 2){
+                    quality.setText("HUBUNGKAN ADAPTOR!");
+                    quality.setTextColor(Color.RED);
+                    dotstatus.setImageResource(R.drawable.offlinedot);
+                    statusadaptor.setText("Adaptor tidak terhubung!");
+                    qualitybg.setBackgroundResource(R.color.default_color);
+                }
+                else if (Float.parseFloat(voltase) > 2 && Float.parseFloat(voltase) < 18.05){
+                    quality.setText("UNDER VOLTAGE :(");
+                    dotstatus.setImageResource(R.drawable.onlinedot);
+                    statusadaptor.setText("Adaptor terhubung!");
+                    quality.setTextColor(Color.parseColor("#FF7A00"));
+                    qualitybg.setBackgroundResource(R.color.uv_color);
+                }
+                else if (Float.parseFloat(voltase) >18.05 && Float.parseFloat(voltase) <19.95){
+                    quality.setText("NORMAL :)");
+                    statusadaptor.setText("Adaptor terhubung!");
+                    dotstatus.setImageResource(R.drawable.onlinedot);
+                    quality.setTextColor(Color.parseColor("#24B400"));
+                    qualitybg.setBackgroundResource(R.color.normal_color);
+                }
+                else {
+                    quality.setText("OVER VOLTAGE :@");
+                    statusadaptor.setText("Adaptor terhubung!");
+                    dotstatus.setImageResource(R.drawable.onlinedot);
+                    quality.setTextColor(Color.RED);
+                    qualitybg.setBackgroundResource(R.color.ov_color);
+                }
             }
 
             @Override
